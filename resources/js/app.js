990d52aa78b802a3defac8e7a3fb40b42a4ad484
saparatayev <sopa.97@mail.ru>
2021-08-03 19:24:16 +0500

    installed vue, downgrade webpack version - incompatible error

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,14 @@
 require('./bootstrap');
 
 require('alpinejs');
+
+// Require Vue
+window.Vue = require('vue').default;
+
+// Register Vue Components
+Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+
+// Initialize Vue
+const app = new Vue({
+    el: '#app',
+});
