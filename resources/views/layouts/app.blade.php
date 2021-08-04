e7977cb09bfa7c463a1694efada736e0fa09d044
saparatayev <sopa.97@mail.ru>
2021-08-04 08:49:02 +0500

    fix open method is not defined

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,7 +18,7 @@
         <script src="{{ asset('js/app.js') }}" defer></script>
     </head>
     <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100" id="app">
+        <div class="min-h-screen bg-gray-100">
             @include('layouts.navigation')
 
             <!-- Page Heading -->
